4f7f5199b3a62f2b1012061fd6ed8ad5417fb71d
bala-amypo <bala@amypo.in>
2025-12-23 08:00:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/AnomalyRuleRepository.java
+++ src/main/java/com/example/demo/repository/AnomalyRuleRepository.java
@@ -1,12 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.AnomalyRule;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
+import com.example.demo.model.AnomalyRule;
 
 public interface AnomalyRuleRepository extends JpaRepository<AnomalyRule, Long> {
-    Optional<AnomalyRule> findByRuleCode(String ruleCode);
-    List<AnomalyRule> findByActiveTrue();
 }
