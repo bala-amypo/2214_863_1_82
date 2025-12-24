7c2fbb89ed3f6659a3d5ea02329a93e6456d854e
bala-amypo <bala@amypo.in>
2025-12-24 06:03:30 +0000

    User commit

--- src/main/java/com/example/demo/dto/EmployeeProfileDto.java
+++ src/main/java/com/example/demo/dto/EmployeeProfileDto.java
@@ -1,1 +1,42 @@
 package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class EmployeeProfileDto {
+
+    private Long id;
+    private String employeeId;
+    private String fullName;
+    private String email;
+    private String teamName;
+    private String role;
+    private Boolean active;
+    private LocalDateTime createdAt;
+
+    public EmployeeProfileDto() {}
+
+    // getters & setters (ALL)
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getEmployeeId() { return employeeId; }
+    public void setEmployeeId(String employeeId) { this.employeeId = employeeId; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTeamName() { return teamName; }
+    public void setTeamName(String teamName) { this.teamName = teamName; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
