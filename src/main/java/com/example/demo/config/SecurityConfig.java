7dc597b0b7419e404410f3bf14207d1e93338f12
bala-amypo <bala@amypo.in>
2025-12-25 16:29:53 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,15 +1,10 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
-
-/**
- * Basic security configuration.
- * Authentication is intentionally simple for college project.
- */
 @Configuration
 public class SecurityConfig {
 
     public SecurityConfig() {
-        // Can be extended with JWT later
+        
     }
 }
