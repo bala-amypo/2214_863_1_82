68b3656719fccfccc4ace2a78534e0c4afc94c9c
bala-amypo <bala@amypo.in>
2025-12-26 21:32:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProductivityMetricRecordRepository.java
+++ src/main/java/com/example/demo/repository/ProductivityMetricRecordRepository.java
@@ -1,15 +1,12 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.ProductivityMetricRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.ProductivityMetricRecord;
-
-@Repository
 public interface ProductivityMetricRecordRepository
         extends JpaRepository<ProductivityMetricRecord, Long> {
 
-    List<ProductivityMetricRecord> findByEmployeeId(Long employeeId);
+    List<ProductivityMetricRecord> findByEmployeeIdIn(List<Long> employeeIds);
 }
